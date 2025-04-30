8940b2d649843a8cfabc69202767b4289a37ea29
DowntownCorilla <leeyunje96@gmail.com>
2025-05-01 01:28:04 +0900

    [design]: 메인화면 이벤트 섹션 추가, 카드배너 아이콘 추가

--- src/app/components/cardBanner/BannerCard.tsx
+++ src/app/components/cardBanner/BannerCard.tsx
@@ -1,11 +1,25 @@
+import Image from 'next/image'
 import './BannerCard.css'
 
-export default function BannerCard({ title, subtitle }: { title: string, subtitle: string }) {
+interface BannerCardProps {
+    title: string;
+    subtitle: string;
+    iconSrc: string;  // 아이콘 경로를 위한 prop 추가
+}
+
+export default function BannerCard({ title, subtitle, iconSrc }: BannerCardProps) {
     return (
         <div className="banner-card">
             <div className="content">
                 <h3>{title}</h3>
                 <p>{subtitle}</p>
+                <Image 
+                    src={iconSrc} 
+                    alt="banner icon" 
+                    width={64} 
+                    height={64} 
+                    className="banner-icon"
+                />
             </div>
         </div>
     )
---
+++ src/app/components/eventSection/eventSection.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import './eventSection.css'
+
+export default function EventSection() {
+    return (
+        <div className="event-section">
+            <Image src="/icons/popcorn.svg" alt="popcorn" width={76} height={76} />
+            <div className="event-section-content">
+                <div>
+                    <p className="event-section-title">
+                        The Netflix you love for just $7.99.
+                    </p>
+                    <p className="event-section-sub_title">
+                        Get our most affordable, ad-supported plan.
+                    </p>
+                </div>
+                <div className="event-section-button">
+                    Learn More
+                </div>
+            </div>
+        </div>
+    )
+}
